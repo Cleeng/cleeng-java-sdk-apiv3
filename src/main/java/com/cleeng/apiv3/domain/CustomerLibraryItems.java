b88a992d0275c0015f4b0d1ee0e06b01903080d3
Mateusz Tymek <mtymek@gmail.com>
2013-11-15 11:37:32 +0100

    Fixed class name

--- src/main/java/com/cleeng/apiv3/domain/CustomerLibraryItems.java
+++ src/main/java/com/cleeng/apiv3/domain/CustomerLibraryItems.java
@@ -9,5 +9,5 @@ public class CustomerLibraryItems implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	public int totalItemCount;
-	public java.util.Vector<Transaction> items;
+	public java.util.Vector<CustomerLibrary> items;
 }
